6c3e7e308da877849e3b8e9d4cca7b805504bcbc
LHY-42 <LHY-42@users.noreply.github.com>
2022-01-28 18:55:05 +0800

    email

--- ourhci/ourhci/settings.py
+++ ourhci/ourhci/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ourhciemails@gmail.com
-EMAIL_HOST_PASSWORD = ourhciemails2022!
+EMAIL_HOST_USER = "ourhciemails@gmail.com"
+EMAIL_HOST_PASSWORD = "ourhciemails2022!"
