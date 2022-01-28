b22f6c99d0960a14c9dc98f7313e5c3a0ef09c96
LHY-42 <LHY-42@users.noreply.github.com>
2022-01-28 19:01:23 +0800

    hello

--- ourhci/ourhci/settings.py
+++ ourhci/ourhci/settings.py
@@ -123,9 +123,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "ourhciemails@gmail.com"
-EMAIL_HOST_PASSWORD = "ourhciemails2022!"
