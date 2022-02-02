1ca9b78f491bc1597c1aa98710f948a7eba58127
RxDefine <64133579+RxDefine@users.noreply.github.com>
2022-02-02 14:54:13 +0800

    Update urls.py
    .

--- ourhci/ourhci/urls.py
+++ ourhci/ourhci/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include("ourapp.urls"))
+    path('', include("ourapp.urls"))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
