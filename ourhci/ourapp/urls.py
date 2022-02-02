22bfa7fe7c4420be633ec3960e969cad19201004
RxDefine <64133579+RxDefine@users.noreply.github.com>
2022-02-02 14:52:45 +0800

    Update urls.py
    changed index path (should work now?)

--- ourhci/ourapp/urls.py
+++ ourhci/ourapp/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("/", views.index, name="index")
+    path("", views.index, name="index")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
