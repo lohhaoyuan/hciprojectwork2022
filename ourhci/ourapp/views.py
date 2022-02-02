2b9bc9f6604e0b8e3b6f6d6924eb5ea25ef5c7e9
LHY-42 <LHY-42@users.noreply.github.com>
2022-02-02 14:43:32 +0800

    "hello '

--- ourhci/ourapp/views.py
+++ ourhci/ourapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import  HttpResponseRedirect
 
 # Create your views here.
 def index(request):
-    
+
     return render(request, "ourapp/index.html")
