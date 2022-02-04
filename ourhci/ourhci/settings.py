a8b27c30e2f65ac098e23e358179f5553f8ace6f
LHY-42 <LHY-42@users.noreply.github.com>
2022-02-04 09:13:39 +0800

    h

--- ourhci/ourhci/settings.py
+++ ourhci/ourhci/settings.py
@@ -106,14 +106,14 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Singapore'
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)q
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
