8b0fa6bdf59b755d7af37463c98264a6742fb5d0
LHY-42 <LHY-42@users.noreply.github.com>
2022-02-04 12:45:27 +0800

    login/logout

--- ourhci/ourapp/urls.py
+++ ourhci/ourapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about")
+    path("about/", views.about, name="about"),
+    path("login/", views.login_view, name="login"),
+    path("register/", views.register, name="register"),
+    path("logout", views.logout_view, name="logout")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
