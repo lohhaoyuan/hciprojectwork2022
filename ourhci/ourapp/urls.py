1697224ec86857541bd196727a88c3acda847e7e
LHY-42 <LHY-42@users.noreply.github.com>
2022-02-12 21:49:23 +0800

    :)

--- ourhci/ourapp/urls.py
+++ ourhci/ourapp/urls.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
+
 urlpatterns = [
+
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("login/", views.login_view, name="login"),
