6b21307f8ae1e1dbebece39fba88ae3c0074bafb
LHY-42 <LHY-42@users.noreply.github.com>
2022-04-04 21:49:01 +0800

    hi"

--- ourhci/ourhci/settings.py
+++ ourhci/ourhci/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "ourapp/static"),
    ]
-MEDIA_URL = ''
+MEDIA_URL = '/ourapp/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
